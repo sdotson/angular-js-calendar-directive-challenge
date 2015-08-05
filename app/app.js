5b3aecfa72488c29a501cbc49fd1e4218de2175f
Stu <stuart@blueprintinteractive.com>
2015-08-04 20:25:34 -0400

    add gulpfile, add gulp dependencies, fix path error to calednarRange.js, fix semicolon issue in app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,9 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+.directive('calendar', calendar);
+
+calendar.$inject = [];
+function calendar() {
+
+}
