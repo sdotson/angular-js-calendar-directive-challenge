2e8a6a4edab63f5115827d0761c54f62afea9473
Stu <stuart@blueprintinteractive.com>
2015-08-05 09:28:35 -0400

    add basic styles, basic calendar view

--- app/app.js
+++ app/app.js
@@ -3,7 +3,19 @@ angular.module('calendarDemoApp', [])
 // your controller and directive code go here
 .directive('calendar', calendar);
 
-calendar.$inject = [];
 function calendar() {
+  calendarController.$inject = ['$scope','$filter'];
+  function calendarController($scope, $filter) {
+    $scope.calendarRange = CalendarRange.getMonthlyRange(new Date());
+    $scope.currentMonth = $filter('date')($scope.calendarRange.start,'M') - 1;
 
+    console.log($scope.calendarRange);
+  }
+
+  return {
+    restrict: 'E',
+    scope: true,
+    templateUrl: 'calendar.html',
+    controller: calendarController
+  };
 }
