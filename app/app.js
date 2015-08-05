264fd4f51c597204529fa9798c9cd089f1089e70
Stu <stuart@blueprintinteractive.com>
2015-08-05 09:54:17 -0400

    add ngOptions for year, begin setup for month

--- app/app.js
+++ app/app.js
@@ -8,8 +8,21 @@ function calendar() {
   function calendarController($scope, $filter) {
     $scope.calendarRange = CalendarRange.getMonthlyRange(new Date());
     $scope.currentMonth = $filter('date')($scope.calendarRange.start,'M') - 1;
+    $scope.currentYear = parseInt($filter('date')($scope.calendarRange.start,'y'));
+    $scope.years = getYears($scope.currentYear);
 
-    console.log($scope.calendarRange);
+    function getYears(year) {
+      var start = year - 20,
+        end = year + 20,
+        yearsArray = [];
+
+      for (var i = start; i < end; i++) {
+        yearsArray.push(start);
+        start++;
+      };
+
+      return yearsArray;
+    }
   }
 
   return {
