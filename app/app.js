4ee0fb7911673bf559ff6b97048a75a6b2f797f7
Stu <stuart@blueprintinteractive.com>
2015-08-06 09:42:43 -0400

    add tests

--- app/app.js
+++ app/app.js
@@ -1,13 +1,40 @@
 (function() {
 
 })();
-
 angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
 .directive('calendar', calendar);
 
 function calendar() {
+  calendarController.$inject = ['$scope','$filter'];
+  function calendarController($scope, $filter) {
+
+    function updateDate(date) {
+      $scope.calendarRange = CalendarRange.getMonthlyRange(date);
+      $scope.currentMonth = $filter('date')($scope.calendarRange.start,'M') - 1;
+      $scope.currentYear = parseInt($filter('date')($scope.calendarRange.start,'y'));
+      $scope.years = getYears($scope.currentYear);  
+    }
+
+    function getYears(year) {
+      var start = year - 20,
+        end = year + 20,
+        yearsArray = [];
+      for (var i = start; i < end; i++) {
+        yearsArray.push(start);
+        start++;
+      };
+      return yearsArray;
+    }
+
+    updateDate(new Date());
+
+    $scope.updateCalendar = function() {
+      updateDate(new Date($scope.selectedYear, $scope.selectedMonth));
+    }
+  }
+  
   return {
     restrict: 'E',
     scope: true,
@@ -16,30 +43,4 @@ function calendar() {
   };
 }
 
-calendarController.$inject = ['$scope','$filter'];
-function calendarController($scope, $filter) {
 
-  function updateDate(date) {
-    $scope.calendarRange = CalendarRange.getMonthlyRange(date);
-    $scope.currentMonth = $filter('date')($scope.calendarRange.start,'M') - 1;
-    $scope.currentYear = parseInt($filter('date')($scope.calendarRange.start,'y'));
-    $scope.years = getYears($scope.currentYear);  
-  }
-
-  function getYears(year) {
-    var start = year - 20,
-      end = year + 20,
-      yearsArray = [];
-    for (var i = start; i < end; i++) {
-      yearsArray.push(start);
-      start++;
-    };
-    return yearsArray;
-  }
-
-  updateDate(new Date());
-
-  $scope.updateCalendar = function() {
-    updateDate(new Date($scope.selectedYear, $scope.selectedMonth));
-  }
-}
